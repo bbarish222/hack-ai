03d71d9c2094f7c8822c6dd13e7a046edb9fc6a5
bbarish222 <107352865+bbarish222@users.noreply.github.com>
2023-01-07 09:24:56 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function hackbtn() {
+  var who = prompt("Who do you want to hack?");
+  var conf = confirm("Are you sure you want to hack " + who + "?");
+  var pass = alert("Password: 12345678");
+}
+function saveTextAsFile(textToWrite, fileNameToSaveAs)
+    {
+    	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'}); 
+    	var downloadLink = document.createElement("a");
+    	downloadLink.download = fileNameToSaveAs;
+    	downloadLink.innerHTML = "Download File";
+    	if (window.webkitURL != null)
+    	{
+    		// Chrome allows the link to be clicked
+    		// without actually adding it to the DOM.
+    		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    	}
+    	else
+    	{
+    		// Firefox requires the link to be added to the DOM
+    		// before it can be clicked.
+    		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    		downloadLink.onclick = destroyClickedElement;
+    		downloadLink.style.display = "none";
+    		document.body.appendChild(downloadLink);
+    	}
+    
+    	downloadLink.click();
+    }
